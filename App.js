d3e4d4b39fe470d0abba7eedbb4a195d56418eb1
Harsh Sharma <harshrajsharma.2003@gmail.com>
2021-09-13 22:57:19 +0530

    UI Complete

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
 import Item from './components/Item';
+
 export default function App() {
+
+  const [task, setTask]=useState("");
+
+  function handleChange(text){
+    setTask(text);
+  }
+  function handleClick(){
+    console.log(task);
+  }
   return (
     <View style={styles.container}>
 
@@ -14,6 +24,18 @@ export default function App() {
           <Item text="Orange" />
         </View>
       </View>
+
+      <KeyboardAvoidingView behavior={Platform.OS==="ios" ? "padding" : 'height' }style={styles.textInputBox}>
+        
+        <TextInput style={styles.input} placeholder="Create task" value={task} onChangeText={handleChange} />
+        
+        <TouchableOpacity onPress={handleClick} >
+          <View style={styles.addBox} >
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+      
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -33,5 +55,32 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop: 30
+  },
+  textInputBox:{
+    position:'absolute',
+    bottom: 60,
+    width: "100%",
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center'
+  }, 
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: "#ffffff",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1
+  },
+  addBox:{
+    width: 60,
+    height: 60,
+    backgroundColor: "#ffffff",
+    borderRadius: 60,
+    justifyContent:'center',
+    alignItems:'center',
+    borderColor: "#C0C0C0",
+    borderWidth: 1
   }
 });
